ad80efaa5e084f273a6b8e1ea2eabe571f2bd76a
0x6c23 <email.dweishaar@gmail.com>
2020-03-17 18:29:41 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -230,8 +230,8 @@ export default class Gantt {
 
         // add date padding on both sides
         if (this.view_is([VIEW_MODE.HOUR, VIEW_MODE.QUARTER_DAY, VIEW_MODE.HALF_DAY])) {
-            this.gantt_start = this.gantt_start; // date_utils.add(this.gantt_start, -7, 'day');
-            this.gantt_end = this.gantt_end; // date_utils.add(this.gantt_end, 7, 'day');
+            this.gantt_start = date_utils.add(this.gantt_start, -1, 'day');
+            this.gantt_end = date_utils.add(this.gantt_end, 1, 'day');
         } else if (this.view_is(VIEW_MODE.MONTH)) {
             this.gantt_start = date_utils.start_of(this.gantt_start, 'year');
             this.gantt_end = date_utils.add(this.gantt_end, 1, 'year');
