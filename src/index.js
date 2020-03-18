f744851eb4d9a466e2782c0a03e5d0c4917133b6
0x6c23 <email.dweishaar@gmail.com>
2020-03-18 10:04:40 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -233,8 +233,8 @@ export default class Gantt {
 
         // add date padding on both sides
         if (this.view_is([VIEW_MODE.HOUR, VIEW_MODE.QUARTER_DAY, VIEW_MODE.HALF_DAY, VIEW_MODE.DAY])) {
-            this.gantt_start =  date_utils.start_of(this.current_day, 'day'); // this.gantt_start; // date_utils.add(this.gantt_start, -1, 'day');
-            this.gantt_end   =  date_utils.add(date_utils.start_of(this.current_day, 'day'), 23, 'hour'); // this.gantt_end === this.gantt_start ? date_utils.add(this.gantt_end, 1, 'day') : this.gantt_end; // date_utils.add(this.gantt_end, 1, 'day');
+            this.gantt_start =  date_utils.add(date_utils.start_of(this.current_day, 'day'), -30, 'minute'); // this.gantt_start; // date_utils.add(this.gantt_start, -1, 'day');
+            this.gantt_end   =  date_utils.add(date_utils.start_of(this.current_day, 'day'), 24, 'hour'); // this.gantt_end === this.gantt_start ? date_utils.add(this.gantt_end, 1, 'day') : this.gantt_end; // date_utils.add(this.gantt_end, 1, 'day');
         } else if (this.view_is(VIEW_MODE.MONTH)) {
             this.gantt_start = date_utils.start_of(this.gantt_start, 'year');
             this.gantt_end = date_utils.add(this.gantt_end, 1, 'year');
