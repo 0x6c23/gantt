96bbb19ef30d163a09eb407339cfa57098176507
0x6c23 <email.dweishaar@gmail.com>
2020-04-08 14:08:44 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -266,7 +266,7 @@ export default class Gantt {
 
         // add date padding on both sides
         if (this.view_is(VIEW_MODE.HOUR)) {
-            this.gantt_start = date_utils.add(this.gantt_start, 1, 'hour'); //date_utils.add(date_utils.start_of(this.current_day, 'day'), -30, 'minute'); // this.gantt_start; // date_utils.add(this.gantt_start, -1, 'day');
+            this.gantt_start = date_utils.add(this.gantt_start, -1, 'hour'); //date_utils.add(date_utils.start_of(this.current_day, 'day'), -30, 'minute'); // this.gantt_start; // date_utils.add(this.gantt_start, -1, 'day');
             this.gantt_end   = date_utils.add(this.gantt_start, 24, 'hour'); //date_utils.add(date_utils.start_of(this.current_day, 'day'), 24, 'hour'); // this.gantt_end === this.gantt_start ? date_utils.add(this.gantt_end, 1, 'day') : this.gantt_end; // date_utils.add(this.gantt_end, 1, 'day');
         } else if (this.view_is([VIEW_MODE.QUARTER_DAY, VIEW_MODE.HALF_DAY])) {
             this.gantt_start =  date_utils.add(this.gantt_start, -7, 'day');
